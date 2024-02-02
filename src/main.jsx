84334708031a33ad6d7984d0d84bc68a90822fdd
Diana <ivdiana14500@gmail.com>
2024-02-02 13:03:37 +0300

    feat: update component

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import './index.css'
+import './styles.module.scss'
 import App from './components/app/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/pages/page-primary/page-primary.jsx
+++ src/pages/page-primary/page-primary.jsx
@@ -3,7 +3,23 @@ import css from "./styles.module.scss";
 function PagePrimary() {
     return (
         <div className={css.page}>
-            <div className={css.overlay}>PagesOne</div>
+            <div className={css.overlay}>
+                <div className={css.content}>
+                    <h1 className={css.title}>
+                        <p className={css.topRight} data-heading="Dream"></p>
+                        <p className={css.topLeft} data-heading="Drea"></p>
+                        <div className={css.bottomTitle}>
+                            <p className={css.bottomRight} data-heading="House"></p>
+                            <p className={css.bottomLeft} data-heading="House"></p>
+                        </div>
+                        <div className={`${css.line} ${css.lineTop}`}></div>
+                        <div className={`${css.line} ${css.lineBottom}`}></div>
+                    </h1>
+                    <h2 className={css.subtitle}>
+                        your personal
+                    </h2>
+                </div>
+            </div>
         </div>
     )
 }
