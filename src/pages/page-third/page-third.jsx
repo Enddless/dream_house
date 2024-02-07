3992667776d75287c24b89fbd8a4eec57de17a57
Diana <ivdiana14500@gmail.com>
2024-02-07 21:09:56 +0300

    feat: update component

--- src/pages/page-third/page-third.jsx
+++ src/pages/page-third/page-third.jsx
@@ -2,7 +2,11 @@ import css from "./styles.module.scss";
 function PageThird() {
     return (
         <div className={css.page}>
-            <div className={css.overlay}>PageThird</div>
+            <div className={css.content}>
+                <p className={css.photo}><span>elite</span></p>
+                <p className={css.photo}><span>vip</span></p>
+                <p className={css.photo}><span>extra</span></p>
+            </div>
         </div>
     )
 }
